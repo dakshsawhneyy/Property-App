a7d9a636f70c64c6cbfa3a3caa40c49345bebab7
Daksh Sawhney <darkstardaksh@gmail.com>
2025-08-05 17:13:20 +0530

    Added PropertyCard Component

---
+++ src/components/propertyCard.jsx
@@ -0,0 +1,13 @@
+const PropertyCard = ({title, location, price, desc, image}) => {
+  return (
+    <div className='border'>
+        <img src={image} className='w-full h-48 object-cover rounded' alt="" />
+        <h2 className='text-lg'>{title}</h2>
+        <h3 className=''>{location}</h3>
+        <p className=''>{desc}</p>
+        <p className=''>{price}</p>
+    </div>
+  )
+}
+
+export default PropertyCard
