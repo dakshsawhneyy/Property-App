07248100393347e898788537059884f5a674bdda
Daksh Sawhney <darkstardaksh@gmail.com>
2025-08-05 17:39:24 +0530

    Added Filtered Properties Feature

--- src/components/propertyCard.jsx
+++ src/components/propertyCard.jsx
@@ -3,9 +3,9 @@ const PropertyCard = ({title, location, price, desc, image}) => {
     <div className='border'>
         <img src={image} className='w-full h-48 object-cover rounded' alt="" />
         <h2 className='text-lg'>{title}</h2>
-        <h3 className=''>{location}</h3>
+        <h3 className=''>Location: {location}</h3>
         <p className=''>{desc}</p>
-        <p className=''>{price}</p>
+        <p className=''>Price: ${price}</p>
     </div>
   )
 }
--- src/pages/PropertyList.jsx
+++ src/pages/PropertyList.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom"
 import PropertyCard from "../components/propertyCard"
+import { useState } from "react";
 
 const PropertyList = () => {
 
@@ -8,11 +9,29 @@ const PropertyList = () => {
 
   const navigate = useNavigate();
 
+  // making states for filtered searches
+  const [locationFilter, setLocationFilter] = useState("")
+  const [minPrice, setMinPrice] = useState("")
+  const [maxPrice, setMaxPrice] = useState("")
+
+  // filter products based on filters used -- to show property, all these properties should be true
+  const filteredProducts = JSON.parse(properties).filter((property) => {
+    const matchLocation = property.location.toLowerCase().includes(locationFilter.toLowerCase());
+    const matchMin = minPrice ? property.price >= minPrice : true;  // if not then allow all
+    const matchMax = maxPrice ? property.price <= maxPrice : true;
+    return matchLocation && matchMin && matchMax;   // product will be shown only when all 3 criteria matches
+  })
+
   return (
     <div>
       <h1 className="text-3xl font-bold my-8">Property List</h1>
+      <div className="flex flex-col md:flex-row gap-5 m-5">
+        <input type="text" placeholder="Search By Location" value={locationFilter} onChange={(e) => setLocationFilter(e.target.value)} className="py-2 px-1 w-[300px] border" />
+        <input type="number" placeholder="minPrice" value={minPrice} onChange={(e) => setMinPrice(e.target.value)} className="py-2 px-1 w-[300px] border" />
+        <input type="number" placeholder="maxPrice" value={maxPrice} onChange={(e) => setMaxPrice(e.target.value)} className="py-2 px-1 w-[300px] border" />
+      </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-        {properties ? JSON.parse(properties).map((item) => (
+        {properties ? filteredProducts.map((item) => (
           <PropertyCard key={item.id} title={item.title} location={item.location} price={item.price} desc={item.description} image={item.image} />
         )) : (
           <p className='text-center'>No properties available</p>
