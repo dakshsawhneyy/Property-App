90efea64e7852646491423b6aecde45a5629ed07
Daksh Sawhney <darkstardaksh@gmail.com>
2025-08-05 18:03:57 +0530

    Fixed unique id issue in add property

--- src/pages/PropertyDetail.jsx
+++ src/pages/PropertyDetail.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useParams } from "react-router-dom"
 
 const PropertyDetail = () => {
+  
+  // With help of use params, fetch id from the url
+  const { id } = useParams();
+  
+  // Fetch the property details from local storage using the id
+  const properties = JSON.parse(localStorage.getItem('properties'));
+  const property = properties.find(p => p.id === id);
+  console.log(property);
+
   return (
-    <div>PropertyDetail</div>
+    <div className="">
+
+    </div>
   )
 }
 
